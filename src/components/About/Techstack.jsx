d155ed5417914c722c7c1d7fe92715d28141cb8a
bhanupattemz <bhanupattemz@gmail.com>
2025-03-08 21:56:50 +0530

    update project and resume

--- src/components/About/Techstack.jsx
+++ src/components/About/Techstack.jsx
@@ -10,6 +10,12 @@ import {
   DiGit,
   DiJava,
 } from "react-icons/di";
+import { FaBootstrap } from "react-icons/fa";
+import { SiEjs } from "react-icons/si";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { TbBrandJavascript } from "react-icons/tb";
+import { SiSqlite } from "react-icons/si";
 import {
   SiRedis,
   SiMysql,
@@ -22,35 +28,52 @@ function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
+        <FaHtml5/>
       </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaCss3Alt/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <TbBrandJavascript />
+      </Col>
+      
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFlask />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiRedux />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMysql />
+        <FaBootstrap />
       </Col>
-
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFlask />
+      </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiEjs/>
+      </Col>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSqlite/>
+      </Col>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
       </Col>
     </Row>
   );
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import chatify from "../../assets/Projects/chatify.png";
 import epic_essentials from "../../assets/Projects/epic_essentials.png"
+import collegeClubs from "../../assets/Projects/collegeClubs.png"
 import yelpCamp from "../../assets/Projects/YelpCamp.png"
-import bikerentals from "../../assets/Projects/bikerentals.png"
+import lessonplanner from "../../assets/Projects/lessonPlanner.png"
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -18,6 +19,17 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <Col md={6} className="project-card">
+            <ProjectCard
+              imgPath={collegeClubs}
+              isBlog={false}
+              title="College Clubs"
+              description="A responsive platform was developed to manage student clubs, events, donations, and announcements, featuring role-based dashboards for admins and coordinators to handle events, members, and club resources. It includes functionalities like event registrations, club polls, a donation system, a library, a gallery, YouTube integration, and a centralized announcement system, with student login enabled via college email ID. The platform was built using HTML, CSS, JavaScript, React, Redux, Material UI, Node.js, Express.js, MongoDB, Cloudinary, Passport.js, Razorpay, and Postman."
+              ghLink="https://github.com/bhanupattemz/CollegeClubs.git"
+              demoLink="https://collegeclubs.onrender.com/"
+            />
+          </Col>
+
           <Col md={6} className="project-card">
             <ProjectCard
               imgPath={epic_essentials}
@@ -26,6 +38,17 @@ function Projects() {
               description="A responsive e-commerce platform has been developed, featuring user authentication, shopping cart functionality, product search, filtering options, reviews, and order tracking, with Stripe integrated as a payment gateway for testing purposes. Additionally, an admin dashboard is being implemented to manage products and users, track orders, and monitor sales in real time, along with inventory management, while ensuring cross-device compatibility."
               ghLink="https://github.com/bhanupattemz/Epic_Essentials.git"
               demoLink="https://epic-essentials.onrender.com/"
+            />
+          </Col>
+          
+          <Col md={6} className="project-card">
+            <ProjectCard
+              imgPath={lessonplanner}
+              isBlog={false}
+              title="Lesson Planner"
+              description="A responsive web application was developed to assist educators in generating, editing, and managing lesson plans using AI, with built-in authentication and session management. It features AI-generated lesson plans based on topic and grade level, customizable lesson editing, PDF export, dark/light theme, and a demo login using local storage and bcrypt for password hashing. The application dynamically adapts lesson content and was built using React.js, ShadCN, TailwindCSS, Google Gemini API, Local Storage, Bcrypt, and PDF generation."
+              ghLink="https://github.com/bhanupattemz/Lesson_Planner.git"
+              demoLink="https://lesson-planner-two.vercel.app/"
             />
           </Col>
 
@@ -39,25 +62,7 @@ function Projects() {
               demoLink="https://yelpcamp-u9q5.onrender.com"
             />
           </Col>
-          <Col md={6} className="project-card">
-            <ProjectCard
-              imgPath={bikerentals}
-              isBlog={false}
-              title="Neighborhood Wheels"
-              description="This dynamic web application was developed for a college coding club to manage bike rentals, built using Firebase and React with a fully responsive design for an optimal user experience across devices. Neighborhood Wheels enables users to list their bikes for rent, while potential renters can browse and rent available bikes with ease."
-              ghLink="https://github.com/bhanupattemz/Neighborhood__wheels-main.git"
-            />
-          </Col>
-          <Col md={6} className="project-card">
-            <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title="Chat App"
-              description="Designed and developed a clean, user-friendly chat interface with real-time messaging, active user status, and notifications, utilizing WebSocket technology via Socket.io for instant communication. The project leverages MongoDB for efficient data storage, optimized through indexing and well-structured data models. It also ensures secure user authentication and authorization using JWT (JSON Web Tokens)."
-              ghLink="https://github.com/bhanupattemz/Chat_App.git"
-            />
-          </Col>
-
+          
 
         </Row>
       </Container>
